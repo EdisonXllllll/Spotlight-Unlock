4168e3d4b8afcd861daf4021fdf927b2594a8ba4
EdisonXllllll <344107415@qq.com>
2021-12-15 18:33:51 +0800

    Add files via upload

--- Siri_Suggestion_response.js
+++ Siri_Suggestion_response.js
@@ -4,13 +4,14 @@ keyListMap.set('suggestion',90);
 keyListMap.set('web',80);
 
 if ($response.status == 200) {
-	let res = JSON.parse($response.body)[0]
-	let result = res.results
+	console.log($response.body);
+	let res = JSON.parse($response.body)[0];
+	let result = res.results;
 
 	result.sort(function(a,b){
-		return keyListMap.get(b.section_key) - keyListMap.get(a.section_key)
+		return keyListMap.get(b.section_key) - keyListMap.get(a.section_key);
 	})
 
-	$done({body:JSON.stringify(result)})
+	$done({body:JSON.stringify(result)});
 }
-$done()
+$done();
