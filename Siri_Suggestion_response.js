824120e1f84da6c392338aeb997a10a4790a4f43
EdisonXllllll <344107415@qq.com>
2021-12-15 19:58:50 +0800

    Update Siri_Suggestion_response.js

--- Siri_Suggestion_response.js
+++ Siri_Suggestion_response.js
@@ -11,7 +11,7 @@ if ($response.status == 200) {
 	for (var i=0;i<result.length;i++)
 	{ 
 	    if(result[i].section_key == 'suggestion'){
-	    	result[i].replacement = 'tophit'
+	    	result[i].placement = 'tophit'
 	    }
 	}
 
